5dcef1ece24a6cda0bf60734e8954c5095d6d1af
Ahmed Rabie <142744167+ahmed3rab5@users.noreply.github.com>
2023-08-31 19:40:45 +0300

    Add files via upload
    A program to fill array with max sizw of 100, With random numbers, And counting only odd numbers in array.

---
+++ problem7.cpp
@@ -0,0 +1,67 @@
+
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int randomNumber(int from, int to) {
+
+    int randnom = (rand() % (to - from + 1)) + from; return randnom;
+
+}
+
+int ReadPositiveNumber(string msg) {
+
+    int number = 0;
+    do
+    {
+        cout << msg << endl;
+        cin >> number;
+
+    } while (number <= 0 || !(cin));
+
+    return number;
+}
+
+void fillArrWithRandomNum(int arr[100], int& arrLen) {
+
+    for (int c = 0; c <= (arrLen - 1); c++)
+        arr[c] = randomNumber(1, 100);
+       
+}
+
+int oddNumber(int arr[100], int arrLen) {
+
+    int countOddNumber = 0;
+    for (int c = 0; c <= (arrLen - 1); c++)
+        if ((arr[c] % 2 != 0))
+           countOddNumber++;
+    return countOddNumber;
+}
+
+void printArrElements(int arr[100], int arrLen) {
+
+    for (int c = 0; c <= (arrLen - 1); c++) 
+        cout << arr[c] << "  ";
+    cout << endl;
+}
+
+int main() {
+
+    srand((unsigned)time(NULL));
+
+    int arr[100]{ 0 }, countOddNumber = 0;
+    int readPosNumber = ReadPositiveNumber("\nEnter the size of array: ");
+    fillArrWithRandomNum(arr, readPosNumber);
+
+    countOddNumber = oddNumber(arr, readPosNumber);
+    cout << "\nArray elements: ";
+
+    printArrElements(arr, readPosNumber);
+    cout << "\nOdd Number in array: " << countOddNumber << endl;
+
+    cout << endl;
+    system("pause"); 
+    return 0;
+}
